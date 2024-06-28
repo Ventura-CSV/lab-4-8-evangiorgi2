746e9f472fbf7936ff8660e760285d90ceda7d74
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 22:16:46 -0700

    if num in range error, divsion error

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main():
+    
+    begin = int(input('Enter a number that a greater than one: '))
+    end = int(input('Enter a value that is greater than the first input: '))
     plist = []
-    ##################################################
-    # Comlete your code here
-    ##################################################
-
+    num = range(begin, end)
+    if num in range:
+        for i in range(2, (num/2)+ 1):
+            if (num % i) == 0:
+                break
+        else:
+            print (f'{num} is a prime number')
+            plist.append(num)
+            
     return plist
 
 
