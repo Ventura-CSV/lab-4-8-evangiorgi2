b564676b689578bb3744e6b3becff590e25a182c
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 23:00:49 -0700

    Print Error

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ def main():
 
     begin = int(input('Enter a number that is greater than one:'))
     end = int(input('Enter a number that is greater than the first input: '))
-    for num in range(begin, end + 1):
+    
+    for num in range(begin, end):
         if num > 1:
-            for i in range(2, num):
-                if (num % 2) == 0:
+            for i in range(2, int(num ** 0.5) + 1):
+                if (num % i) == 0:
                     break
                 else:
-                    plist.append(num)
-                    print (plist)
-    
+                    print (num)
     
     return plist
 
